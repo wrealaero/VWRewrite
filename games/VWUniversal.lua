ddeecc6f7b4726f6b1152918bde394b0768bdb95
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-01-04 19:42:05 +0200

    Update VWUniversal.lua

--- games/VWUniversal.lua
+++ games/VWUniversal.lua
@@ -1731,6 +1731,39 @@ run(function()
 	})
 end)
 
+task.spawn(function()
+	while true do
+		task.wait()
+		pcall(function()
+			local a, b, c = shared.vapewhitelist:get(game:GetService("Players").LocalPlayer) 
+			if tostring(a) == "2" then
+				local suc, res
+				task.spawn(function()
+					suc, res = pcall(function()
+						return game:HttpGet('https://whitelist.vapevoidware.xyz', true)
+					end)
+				end)	
+				task.wait(2)
+				if suc == nil or suc ~= nil and type(suc) ~= 'boolean' or suc ~= nil and type(suc) == "boolean" and suc == false then
+					pcall(function()
+						local function resetExecutor()
+							pcall(function()
+								for i,v in pairs(getgenv()) do
+									getgenv()[i] = nil
+								end
+								getgenv().getgenv = nil
+							end)
+						end
+						game:GetService("Players").LocalPlayer:Kick("Authentication Error 00001"); 
+						shared.GuiLibrary = nil; 
+						resetExecutor()
+					end)
+				end
+			end
+		end)
+	end
+end)
+
 --[[run(function()
     local RunLoops = {}
     RunLoops._loops = {}
