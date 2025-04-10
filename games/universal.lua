87887effa101e4f6d2bebb6a706a6ea03fdd8ae8
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-04-10 20:50:58 +0300

    Update universal.lua

--- games/universal.lua
+++ games/universal.lua
@@ -792,7 +792,7 @@ run(function()
 	end
 
 	local function pDecode(data)
-		local suc, err = pcall(function() game:GetService("HttpService"):JSONDecode(data) end)
+		local suc, err = pcall(function() return game:GetService("HttpService"):JSONDecode(data) end)
 		return suc and err
 	end
 
