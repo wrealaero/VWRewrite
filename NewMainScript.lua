933b424fa4159a32bc1b4b228195db30bbb65155
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-05-23 21:15:20 +0300

    AC Mod View & Autowin Features! 🚀

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -215,7 +215,7 @@ if not are_installed_1() then pcall(function() install_profiles(1) end) end
 local url = shared.RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VWRewrite"
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = 'df48d834568e673cc801630e67b2bfb9e77d8bfb'
+commit = 'bf9c616ef129a67360d591d463ed5569453c4f04'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
