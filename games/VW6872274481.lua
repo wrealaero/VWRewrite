8c87a28df229e6fa43f82e03f7e3cc15563453fc
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-05-25 20:47:00 +0300

    Update VW6872274481.lua

--- games/VW6872274481.lua
+++ games/VW6872274481.lua
@@ -5293,6 +5293,7 @@ pcall(function()
 				StaffDetector.Enabled = enabled
 				if enabled then
 					for _, player in pairs(PlayersService:GetPlayers()) do
+						if player == PlayersService.LocalPlayer then continue end
 						DetectionMethods.scanPlayer(player)
 					end
 					
