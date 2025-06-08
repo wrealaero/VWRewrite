575fb53c7a54319302fc56d2c3d52c3acc991fae
Erchobg <67239055+Erchobg@users.noreply.github.com>
2025-06-08 18:29:09 +0300

    Update NewMainScript.lua

--- NewMainScript.lua
+++ NewMainScript.lua
@@ -215,7 +215,7 @@ if not are_installed_1() then pcall(function() install_profiles(1) end) end
 local url = shared.RiseMode and "https://github.com/VapeVoidware/VWRise/" or "https://github.com/VapeVoidware/VWRewrite"
 local commit = "main"
 writefile(baseDirectory.."commithash2.txt", commit)
-commit = 'e2d676bb6a4dd327e104de6727d828b35a4c76eb'
+commit = '1e4a3a250117b0200aba8cbbbb5f0bb5aa156c12'
 commit = shared.CustomCommit and tostring(shared.CustomCommit) or commit
 writefile(baseDirectory.."commithash2.txt", commit)
 local function vapeGithubRequest(scripturl, isImportant)
